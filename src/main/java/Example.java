e3a6d85ffe0fee211e0e7f2f4b0ff8d5e0ce3b40
Вольф <wgs.777@yandex.ru>
2019-01-22 06:57:28 +0300

    vxxbzvcv

---
+++ src/main/java/Example.java
@@ -0,0 +1,15 @@
+public class Example {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
